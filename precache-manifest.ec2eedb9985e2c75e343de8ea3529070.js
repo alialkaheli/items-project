db866a7bace4cc12af3a9b5b2c3109017553c4a1
ali alkaheli <ali.alkaheli96@gmail.com>
2019-10-28 22:00:22 -0700

    Updates

---
+++ precache-manifest.ec2eedb9985e2c75e343de8ea3529070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bdef2bb63aa5002f09d8e746fdf498",
+    "url": "/items-project/index.html"
+  },
+  {
+    "revision": "0651b885cffc61100be1",
+    "url": "/items-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "04fc92687c9e8a58fd48",
+    "url": "/items-project/static/js/2.0eccf72f.chunk.js"
+  },
+  {
+    "revision": "0651b885cffc61100be1",
+    "url": "/items-project/static/js/main.1f8b8516.chunk.js"
+  },
+  {
+    "revision": "383379cc10ead069e807",
+    "url": "/items-project/static/js/runtime-main.a7693a2f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/items-project/static/media/logo.25bf045c.svg"
+  }
+]);
